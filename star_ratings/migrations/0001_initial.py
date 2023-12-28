0a73ff632989396f2540d3c7d42bf36725109405
Animesh Nepal <animesh.nepal@olenepal.org>
2023-12-28 16:12:41 +0545

    uuid-compatible

--- star_ratings/migrations/0001_initial.py
+++ star_ratings/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from decimal import Decimal
 import model_utils.fields
 import django.utils.timezone
 from django.conf import settings
-
+import uuid
 dependancies = [
     migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ('contenttypes', '0001_initial'),
@@ -25,11 +25,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Rating',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('count', models.PositiveIntegerField(default=0)),
                 ('total', models.PositiveIntegerField(default=0)),
                 ('average', models.DecimalField(decimal_places=3, max_digits=6, default=Decimal('0'))),
-                ('object_id', models.PositiveIntegerField(blank=True, null=True)),
+                ('object_id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('content_type', models.ForeignKey(blank=True, null=True, to='contenttypes.ContentType', on_delete=models.CASCADE)),
             ],
             options={
